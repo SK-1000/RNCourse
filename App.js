5850f2138e450c9939a41905fc2248f1ab8a16d2
sk1001 <sheilafleming81@gmail.com>
2024-01-08 11:15:56 +0000

    rectified issue with comments

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+// author Sheila Kirwan 7th January 2024
+// This app allows the user to add and later delete a list of goals using React Native and expo go.
+
+
+
 import { useState } from "react";
 import { StyleSheet, View, FlatList, Button } from "react-native";
 import{StatusBar } from 'expo-status-bar'
@@ -5,8 +10,30 @@ import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
+
+  //state hooks
   const [modalIsVisable, setModalIsVisible] = useState(false);
   const [courseGoals, setCourseGoals] = useState([]);
+
+
+  // Explanation:
+
+  // Defining a State Variable:
+  
+  // modalIsVisible: This is like a special box where your React Native app can keep track of whether a modal (a pop-up or special area on the screen) is currently visible or not.
+  // Setting an Initial Value:
+  
+  // useState(false): This part says, "Start with the modal not visible." So, when your app begins, the special box (modalIsVisible) is set to false, meaning the modal is initially hidden.
+  // Creating a Function to Update the State:
+  
+  // setModalIsVisible: This is like a special tool that React Native gives you. When you want to make the modal visible or hidden, you use this tool.
+  // Using the State Variable and Function:
+  
+  // The const [modalIsVisible, setModalIsVisible] part is like saying, "Create a special box called modalIsVisible and a tool called setModalIsVisible that I can use to open or close that box."
+
+
+
+
 
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
@@ -30,6 +57,11 @@ export default function App() {
     });
   }
 
+
+
+
+  // GoalInput is imported from another component file. This is like a custom component in your React Native app. 
+  //It's a way of saying, "I've created a special area or tool for handling input related to goals."
   return (
     <>
     <StatusBar style="light" />
@@ -39,6 +71,7 @@ export default function App() {
         color="#5e0acc"
         onPress={startAddGoalHandler}
       />
+
       <GoalInput
         visible={modalIsVisable}
         onAddGoal={addGoalHandler}
